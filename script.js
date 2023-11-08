5ea94d40585c57c21c177c6cac20f5ea469a2673
RAJANIKANTA BISWAL <rajanikantabiswal15@gmail.com>
2023-11-08 18:02:42 +0530

    2 project done

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+const body= document.querySelector("body");
+const buttons= document.querySelectorAll(".button");
+const reset_btn= document.querySelector('.reset-btn');
+console.log(body);
+console.log(buttons);
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+        console.log(e.target);
+        console.log(e.target.id);
+        if(e.target.id=== 'red'){
+            body.style.backgroundColor= e.target.id;
+        }
+        if(e.target.id=== 'yellow'){
+            body.style.backgroundColor= e.target.id;
+        }
+        if(e.target.id=== 'green'){
+            body.style.backgroundColor= e.target.id;
+        }
+        if(e.target.id=== 'aqua'){
+            body.style.backgroundColor= e.target.id;
+        }
+    })
+});
+console.log(reset_btn);
+reset_btn.addEventListener('click', function(e){
+console.log(e.target.id);
+if(e.target.id==='reset'){
+    body.style.backgroundColor="white";
+}
+});
 
-const parent=document.getElementsByClassName("parent");
-console.log(parent);
-console.log(parent.children);
 
 
 
